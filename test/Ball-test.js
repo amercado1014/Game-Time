b5376aa3462fbd3aa9cd9aa46fc7e502c905d569
Aldo Mercado <amercado1014@gmail>
2018-01-31 14:42:20 -0700

    Finish Ball and Brick testing
    Added Ball and Brick collision tests.

--- test/Ball-test.js
+++ test/Ball-test.js
@@ -1,11 +1,12 @@
 const { assert } = require('chai');
 const Ball = require('../lib/Ball.js');
+const Game = require('../lib/Game.js');
 
 describe('Ball', function() {
   var ball;
 
   beforeEach(function() {
-    ball = new Ball(50, 50, 10, 2, 2);
+    ball = new Ball(20, 560, 15, 5, 5);
   });
 
   it('should return true', function() {
@@ -13,11 +14,11 @@ describe('Ball', function() {
   });
 
   it('should take arguments for x, y, radius, dX and dY', function() {
-    assert.equal(ball.x, 50);
-    assert.equal(ball.y, 50);
-    assert.equal(ball.radius, 10);
-    assert.equal(ball.dX, 2);
-    assert.equal(ball.dY, -2);
+    assert.equal(ball.x, 20);
+    assert.equal(ball.y, 560);
+    assert.equal(ball.radius, 15);
+    assert.equal(ball.dX, 5);
+    assert.equal(ball.dY, -5);
   });
 
   it('should have a default startAngle and endAngle', function() {
@@ -29,7 +30,87 @@ describe('Ball', function() {
     assert.equal(ball.playing, false);
   });
 
-  it('should start with three lives', function() {
-    assert.equal(ball.lives, 3);
+  it('should move on canvas', function() {
+    var game = new Game;
+
+    assert.equal(ball.x, 20);
+    assert.equal(ball.y, 560);
+
+    ball.bounce(game);
+
+    assert.equal(ball.x, 25);
+    assert.equal(ball.y, 555);
+
+  });
+
+  it('should change horizontal direction when it hits the sides of the canvas', function() {
+    var game = new Game;
+
+    assert.equal(ball.dX, 5);
+    ball.bounce(game);
+    assert.equal(ball.dX, 5);
+
+    ball = new Ball(840, 100, 15, 5, 5);
+
+    assert.equal(ball.dX, 5);
+    ball.bounce(game);
+    assert.equal(ball.dX, -5);
+  });
+
+  it('should change vertical direction when it hits the top of the canvas', function() {
+    var game = new Game;
+
+    assert.equal(ball.dY, -5);
+    ball.bounce(game);
+    assert.equal(ball.dY, -5);
+
+    ball = new Ball(20, 10, 15, 5, 5);
+
+    assert.equal(ball.dY, -5);
+    ball.bounce(game);
+    assert.equal(ball.dY, 5);
+  });
+
+  it('should decrement lives if not hit by paddle', function() {
+    var game = new Game;
+
+    assert.equal(game.lives, 3);
+    ball.bounce(game);
+    assert.equal(game.lives, 3);
+
+    ball = new Ball(20, 700, 15, 5, 5);
+
+    assert.equal(game.lives, 3);
+    ball.bounce(game);
+    assert.equal(game.lives, 2);
+  });
+
+  it('should repositon after losing a life', function() {
+    var game = new Game;
+
+    assert.equal(game.lives, 3);
+    ball.bounce(game);
+    assert.equal(game.lives, 3);
+
+    ball = new Ball(20, 700, 15, 5, 5);
+
+    assert.equal(game.lives, 3);
+    ball.bounce(game);
+    assert.equal(game.lives, 2);
+
+    assert.equal(ball.x, 420);
+    assert.equal(ball.y, 560);
+    assert.equal(ball.dY, 5);
+  });
+
+  it('should stop playing if lives equal 0', function() {
+    var game = new Game;
+    ball = new Ball(20, 700, 15, 5, 5);
+    game.lives = 1
+    
+    assert.equal(game.lives, 1);
+    ball.bounce(game);
+    assert.equal(game.lives, 0);
+    assert.equal(ball.playing, false);
   });
 });
